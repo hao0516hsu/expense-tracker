b5c039c21f47a32a91f3bcad7acac44d480a7478
hao0516hsu <chhsu0516@gmail.com>
2023-06-23 15:34:32 +0800

    feat: add filter function

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ if (process.env.DOTENV !== 'production') {
   require('dotenv').config()
 }
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 const db = mongoose.connection
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
+// GET路由: 首頁
 router.get('/', (req, res) => {
   // 先找出Category的資料，結果存到categories
   Category.find()
@@ -13,6 +14,7 @@ router.get('/', (req, res) => {
         // populate要以'使用Schema.Types.ObjectId'的欄位名稱
         .populate('categoryId')
         .lean()
+        .sort({ date: 'desc' })
         .then(records => {
           let totalAmount = 0
 
@@ -30,6 +32,57 @@ router.get('/', (req, res) => {
           res.render('index', { categories, records, totalAmount })
         })
     })
+})
+
+// GET路由: 首頁+搜尋
+router.post('/', (req, res) => {
+  // 取出Select Menu的值
+  const { categoryName } = req.body
+
+  // 1-a. 若沒有選取，跳轉回首頁
+  if (!categoryName) {
+    res.redirect('/')
+    // 1-b. 有選取的狀況 
+  } else {
+    // 先找出Category的資料，結果存到categories
+    Category.find()
+      .lean()
+      .then(categories => {
+        // 將Select Menu的categoryName給到categoryId
+        let categoryId = ''
+
+        categories.forEach(category => {
+          if (category.name === categoryName) {
+            categoryId = category._id
+            category['isChoosed'] = true
+          }
+        })
+
+        // 找出Record的資料，結果存到records
+        Record.find({ categoryId })
+          // populate要以'使用Schema.Types.ObjectId'的欄位名稱
+          .populate('categoryId')
+          .lean()
+          .sort({ date: 'desc' })
+          .then(records => {
+            let totalAmount = 0
+            // 判斷該項為奇偶數, 並新增屬性到records
+            records.map((record, index) => {
+              if (index % 2) {
+                records[index]['isEven'] = false
+              } else {
+                records[index]['isEven'] = true
+              }
+              // 計算花費總額(totalAmount)
+              totalAmount += record.amount
+            })
+            // 將categories, records, totalAmount傳到handlebars
+            res.render('index', { categories, records, totalAmount, categoryName })
+          })
+          .catch(err => console.log(err))
+      })
+      .catch(err => console.log(err))
+  }
 })
 
 module.exports = router
