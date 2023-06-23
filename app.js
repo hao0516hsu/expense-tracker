32dd2a2467d5b12dabdac2c269bbe14b5921c64b
hao0516hsu <chhsu0516@gmail.com>
2023-06-23 16:19:57 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+// Body-Parser: 取得POST的資料(req.body)
 const bodyParser = require('body-parser')
+// Method-Override: RESTful API相關
+const methodOverride = require('method-override')
 
+// 引用路由
 const routes = require('./routes/index')
-
+// DOTENV設定
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-// Mongoose
+// 引用Mongoose
 require('./config/mongoose')
 
 const app = express()
 const port = process.env.PORT
+
 // Handlebars設定
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-// Body-parser
+// Body-parser設定
 app.use(bodyParser.urlencoded({ extended: true }))
-
+// Method-override設定
+app.use(methodOverride('_method'))
+// 使用routes
 app.use(routes)
 
 app.listen(port, () => {
--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -3,6 +3,27 @@ const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
+// 函式庫
+const utilities = {
+  ConvertToSlashDate(dashDate){
+    return new Date(dashDate).toLocaleDateString('zh-TW')
+  },
+  ConvertToDashDate(slashDate){
+    // 1. 取出日期，並將年月日分開存入陣列
+    const date = new Date(slashDate)
+    const dateArray = [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+    // 2.判斷是否需要補0
+    dateArray.map((value, index) => {
+      if (value.toString().length < 2) {
+        value = '0' + value.toString()
+        dateArray.splice(index, 1, value)
+      }
+    })
+    // 3. 重組成字串
+    return dateArray.join('-')
+  }
+}
+
 // Get路由：新增頁(new)
 router.get('/new', (req, res) => {
   // 先找出Category的資料，結果存到categories
@@ -18,16 +39,17 @@ router.post('/', (req, res) => {
   const { name, date, categoryName, amount } = req.body
   const userId = "6492b4eb257d00d8a027bd08" //測試資料，等登入功能完成後修改
   // 將日期從YYYY-MM-DD轉成YYYY/MM/DD
-  let dateTW = new Date(date).toLocaleDateString('zh-TW')
+  let slashDate = utilities.ConvertToSlashDate(date)
 
   Category.findOne({ name: categoryName })
     .lean()
     .then(category => {
+      // 先到Category取得categoryId，再新增資料
       let categoryId = category._id
 
       Record.create({
         name,
-        date: dateTW,
+        date: slashDate,
         amount,
         userId,
         categoryId
@@ -49,43 +71,32 @@ router.get('/:id', (req, res) => {
         .lean()
         .then(record => {
           // 將日期從YYYY/MM/DD轉成YYYY-MM-DD 給HTML的value
-          // 1. 取出日期，並將年月日分開存入陣列
-          const date = new Date(record.date)
-          const dateArray = [date.getFullYear(), date.getMonth() + 1, date.getDate()]
-          // 2.判斷是否需要補0
-          dateArray.map((value, index) => {
-            if (value.toString().length < 2) {
-              value = '0' + value.toString()
-              dateArray.splice(index, 1, value)
-            }
-          })
-          // 3. 重組成字串
-          dateForHb = dateArray.join('-')
+          dashDate = utilities.ConvertToDashDate(record.date)      
 
           categories.map((category, index) => {
             if (category.name === record.categoryId.name) {
               categories[index]['isChoosed'] = true
             }
           })
-          res.render('edit', { categories, record, dateForHb })
+          res.render('edit', { categories, record, dashDate })
         })
         .catch(err => console.log(err))
     })
     .catch(err => console.log(err))
 })
 
-// Post路由：編輯頁(edit)
-router.post('/:id', (req, res) => {
+// Put路由：編輯頁(edit)
+router.put('/:id', (req, res) => {
   const id = req.params.id
   const { name, date, categoryName, amount } = req.body
-  let dateTW = new Date(date).toLocaleDateString('zh-TW')
+  let slashDate = utilities.ConvertToSlashDate(date)
 
   Category.findOne({ name: categoryName })
     .then(category => {
       Record.findById(id)
         .then(record => {
           record.name = name
-          record.date = dateTW
+          record.date = slashDate
           record.amount = amount
           record.userId = "6492b4eb257d00d8a027bd08"
           record.categoryId = category._id
@@ -98,8 +109,8 @@ router.post('/:id', (req, res) => {
     .catch(err => console.log(err))
 })
 
-// Post路由：刪除頁(delete)
-router.post('/:id/delete',(req,res)=>{
+// Delete路由：刪除頁(delete)
+router.delete('/:id',(req,res)=>{
   const id = req.params.id
 
   Record.findById(id)
