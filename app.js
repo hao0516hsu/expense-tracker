e5c8c77f56b8f77ad9561df1aa36a1524ad11b2d
hao0516hsu <chhsu0516@gmail.com>
2023-06-24 16:24:23 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 // Method-Override: RESTful API相關
 const methodOverride = require('method-override')
+// Express-Session: 產生Session
+const session = require('express-session')
 
-// 引用路由
-const routes = require('./routes/index')
 // DOTENV設定
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
+// 引用路由
+const routes = require('./routes')
 // 引用Mongoose
 require('./config/mongoose')
 
@@ -24,6 +27,13 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 // Method-override設定
 app.use(methodOverride('_method'))
+// Express-session設定
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
 // 使用routes
 app.use(routes)
 
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,14 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 // Get路由: 登入頁(login)
-router.get('/login',(req,res)=>{
+router.get('/login', (req, res) => {
   res.render('login')
 })
+
+// Post路由: 登入頁(login)
+router.post('/login', (req, res) => { })
 
 // Get路由: 註冊頁(register)
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+// Post路由: 註冊頁(register)
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('This email has already been registered.')
+
+        res.render('register', { name, email, password, confirmPassword })
+      } else {
+        User.create({ name, email, password })
+          .then(()=>res.redirect('/users/login'))
+          .catch(err=>console.log(err))
+      }
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
